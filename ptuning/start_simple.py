674ae28beac56cb0ba1e17c0a116099f9ebc6bb1
qingyuan18 <qingyuan18@gmail.com>
2023-05-05 17:53:08 +0800

    add adaption with SageMaker

--- ptuning/start_simple.py
+++ ptuning/start_simple.py
@@ -25,4 +25,4 @@ if __name__ == "__main__":
     #Note: we will use the pytorch launcher to launch deepspeed for multi-nodes training.
     #Note: we will use the s5cmd to speed up the uploading model assets to S3.
     os.system("chmod +x ./s5cmd")
-    os.system("/bin/bash train_simple.sh")
+    os.system("/bin/bash train-simple.sh")
